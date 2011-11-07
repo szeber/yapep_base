da27817fd45ac87caa5c9ccc6c484b9feebb5ce0
Zsolt Szeberenyi <szeber@yapep.org>
2011-11-07 23:05:25 +0100

    Removed version phpdoc tag

--- YapepBase/Router/IRouter.php
+++ YapepBase/Router/IRouter.php
@@ -7,7 +7,6 @@
  * @author       Zsolt Szeberenyi <szeber@yapep.org>
  * @copyright    2011 The YAPEP Project All rights reserved.
  * @license      http://www.opensource.org/licenses/bsd-license.php BSD License
- * @version      $Rev$
  */
 
 
