16f9be5d7e390847cc92ce05a418c608bd136206
Janos Pasztor <j.pasztor@ixolit.com>
2011-12-12 09:48:25 +0100

    Added catch if syslog socket can't be created

--- Test/Syslog/NativeSyslogConnectionTest.php
+++ Test/Syslog/NativeSyslogConnectionTest.php
@@ -12,7 +12,7 @@ class NativeSyslogConnectionTest extends \PHPUnit_Framework_TestCase {
      * @var NativeSyslogConnection
      */
     protected $object;
-    
+
     protected $logpath;
     protected $sock;
     protected $dgram;
@@ -20,25 +20,29 @@ class NativeSyslogConnectionTest extends \PHPUnit_Framework_TestCase {
     protected function setUp() {
         $this->object = new NativeSyslogConnection();
     }
-    
+
     protected function initSyslogServer($logpath, $dgram = false) {
-        if (\file_exists($logpath)) {
-            \unlink($logpath);
+        try {
+            if (\file_exists($logpath)) {
+                \unlink($logpath);
+            }
+            if ($dgram) {
+                $this->sock = \socket_create(AF_UNIX, SOCK_DGRAM, 0);
+            } else {
+                $this->sock = \socket_create(AF_UNIX, SOCK_STREAM, 0);
+            }
+            \socket_set_option($this->sock, SOL_SOCKET, SO_REUSEADDR, 1);
+            \socket_bind($this->sock, $logpath);
+            if (!$dgram) {
+                \socket_listen($this->sock);
+            }
+            $this->logpath = $logpath;
+            $this->dgram = $dgram;
+        } catch (\PHPUnit_Framework_Error_Warning $e) {
+            $this->markTestSkipped('Unable to create log socket');
         }
-        if ($dgram) {
-            $this->sock = \socket_create(AF_UNIX, SOCK_DGRAM, 0);
-        } else {
-            $this->sock = \socket_create(AF_UNIX, SOCK_STREAM, 0);
-        }
-        \socket_set_option($this->sock, SOL_SOCKET, SO_REUSEADDR, 1);
-        \socket_bind($this->sock, $logpath);
-        if (!$dgram) {
-            \socket_listen($this->sock);
-        }
-        $this->logpath = $logpath;
-        $this->dgram = $dgram;
     }
-    
+
     protected function getSyslogMessage() {
         if (!$this->dgram) {
             $client = \socket_accept($this->sock);
@@ -54,9 +58,9 @@ class NativeSyslogConnectionTest extends \PHPUnit_Framework_TestCase {
         }
         return false;
     }
-    
+
     protected function closeSyslogServer() {
-        \socket_close($this->sock);        
+        \socket_close($this->sock);
         if (\file_exists($this->logpath)) {
             \unlink($this->logpath);
         }
@@ -66,26 +70,26 @@ class NativeSyslogConnectionTest extends \PHPUnit_Framework_TestCase {
         $this->object->setPath('/test');
         $this->assertEquals('/test', $this->object->getPath());
     }
-    
+
     public function testIdent() {
         $this->object->setIdent('identtest');
         $this->assertEquals('identtest', $this->object->getIdent());
     }
-    
+
     public function testOptions() {
         $this->object->setOptions(ISyslogConnection::LOG_PID);
         $this->assertEquals(ISyslogConnection::LOG_PID, $this->object->getOptions());
     }
-    
+
     public function testFacility() {
         $this->object->setFacility(Syslog::LOG_AUTH);
         $this->assertEquals(Syslog::LOG_AUTH, $this->object->getFacility());
-        
+
         try {
             $this->object->setFacility(192);
             $this->fail('Setting an invalid facility should result in a ParameterException');
         } catch (\YapepBase\Exception\ParameterException $e) { }
-        
+
         try {
             $this->object->setFacility(-1);
             $this->fail('Setting an invalid facility should result in a ParameterException');
@@ -105,26 +109,26 @@ class NativeSyslogConnectionTest extends \PHPUnit_Framework_TestCase {
 
         $this->initSyslogServer($logpath);
         $this->object->open();
-        
+
         $this->object->log(Syslog::LOG_NOTICE, 'test', 'test', mktime(15, 45, 19, 12, 6, 2011));
         $this->assertEquals('<13>Dec  6 15:45:19 test: test', $this->getSyslogMessage());
         $this->object->close();
         $this->closeSyslogServer();
     }
-    
+
     public function testLoggingWithNoParams() {
         $logpath = \dirname(__DIR__) . '/Temp/Syslog/log';
 
         $this->initSyslogServer($logpath);
         $this->object->setPath($logpath);
         $this->object->open();
-        
+
         $this->object->log(Syslog::LOG_NOTICE, 'test');
         $this->assertRegExp('/^<13>(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) ( |1|2|3)([0-9]) ([0-9]{2}):([0-9]{2}):([0-9]{2}) php: test$/', $this->getSyslogMessage());
-        $this->object->close();        
+        $this->object->close();
         $this->closeSyslogServer();
     }
-    
+
     public function testHandleError() {
         try {
             $this->object->setPath('/nonexistent');
@@ -132,7 +136,7 @@ class NativeSyslogConnectionTest extends \PHPUnit_Framework_TestCase {
             $this->fail('Connecting to a non-existent socket should result in a SyslogException');
         } catch (\YapepBase\Syslog\SyslogException $e) { }
     }
-    
+
     public function testInvalidParams() {
         try {
             $this->object->setIdent('Test Ident');
@@ -155,7 +159,7 @@ class NativeSyslogConnectionTest extends \PHPUnit_Framework_TestCase {
             $this->fail('Float priority should result in a SyslogException');
         } catch (\YapepBase\Syslog\SyslogException $e) { }
     }
-    
+
     public function testDgramSockets() {
         $logpath = \dirname(__DIR__) . '/Temp/Syslog/log';
         $this->object->setFacility(Syslog::LOG_USER);
@@ -163,23 +167,23 @@ class NativeSyslogConnectionTest extends \PHPUnit_Framework_TestCase {
         $this->object->setIdent('test');
 
         $this->initSyslogServer($logpath, true);
-        
+
         $this->object->log(Syslog::LOG_NOTICE, 'test', 'test', mktime(15, 45, 19, 12, 6, 2011));
         $this->assertEquals('<13>Dec  6 15:45:19 test: test', $this->getSyslogMessage());
-        $this->object->close();        
+        $this->object->close();
         $this->closeSyslogServer();
     }
-    
+
     public function testLegacyCalls() {
         $logpath = \dirname(__DIR__) . '/Temp/Syslog/log';
         $this->object->setPath($logpath);
 
         $this->initSyslogServer($logpath, true);
         $this->object->openlog('test', 0, Syslog::LOG_LOCAL0);
-        
+
         $this->object->syslog(Syslog::LOG_NOTICE, 'test');
         $this->assertRegExp('/^<133>(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) ( |1|2|3)([0-9]) ([0-9]{2}):([0-9]{2}):([0-9]{2}) test: test$/', $this->getSyslogMessage());
-        $this->object->closelog();        
+        $this->object->closelog();
         $this->closeSyslogServer();
     }
 }
