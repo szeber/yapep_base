ecfd4f7433d1ed90a1c7dd2cc3ad37ccb0fb5b0f
Zsolt Szeberenyi <z.szeberenyi@ixolit.com>
2011-12-14 09:48:20 +0000

    Added event handling

---
+++ Event/Event.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * This file is part of YAPEPBase.
+ *
+ * @package      YapepBase
+ * @subpackage   Event
+ * @author       Zsolt Szeberenyi <szeber@yapep.org>
+ * @copyright    2011 The YAPEP Project All rights reserved.
+ * @license      http://www.opensource.org/licenses/bsd-license.php BSD License
+ */
+
+namespace YapepBase\Event;
+
+/**
+ * Event class
+ *
+ * @package    YapepBase
+ * @subpackage Event
+ */
+class Event {
+
+    /** Application start event type. */
+    const TYPE_APPSTART = 'application.start';
+    /** Application finish event tyoe. */
+    const TYPE_APPFINISH = 'application.finish';
+    /** Controller before action event type. */
+    const TYPE_CONTROLLER_BEFORE = 'controller.beforeAction';
+    /** Controller after action event type. */
+    const TYPE_CONTROLLER_AFTER = 'controller.afterAction';
+
+    /**
+     * The event's type
+     *
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * Stores the data specific to the event in an associative array.
+     *
+     * @var array
+     */
+    protected $data = array();
+
+    /**
+     * Constructor
+     *
+     * @param string $type
+     * @param array  $data
+     */
+    public function __construct($type, array $data = array()) {
+        $this->type = $type;
+        $this->data = $data;
+    }
+
+    /**
+     * Returns the event type.
+     *
+     * @return string
+     */
+    public function getType() {
+        return $this->type;
+    }
+
+    /**
+     * Returns the data for the event.
+     *
+     * @return array
+     */
+    public function getData() {
+        return $this->data;
+    }
+
+}
---
+++ Event/EventHandlerRegistry.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * This file is part of YAPEPBase.
+ *
+ * @package      YapepBase
+ * @subpackage   Event
+ * @author       Zsolt Szeberenyi <szeber@yapep.org>
+ * @copyright    2011 The YAPEP Project All rights reserved.
+ * @license      http://www.opensource.org/licenses/bsd-license.php BSD License
+ */
+
+namespace YapepBase\Event;
+use YapepBase\Exception\Exception;
+
+/**
+ * EventHandlerRegistry class
+ *
+ * @package    YapepBase
+ * @subpackage Event
+ */
+class EventHandlerRegistry {
+
+    /**
+     * Stores the event handler instances
+     *
+     * @var array
+     */
+    protected $eventHandlers = array();
+
+    /**
+     * Registers a new event handler for the given event type
+     *
+     * @param string                         $eventType
+     * @param \YapepBase\Event\IEventHandler $eventHandler
+     */
+    public function registerEventHandler($eventType, IEventHandler $eventHandler) {
+        if (!isset($this->eventHandlers[$eventType])) {
+            $this->eventHandlers[$eventType] = array();
+        }
+        $this->eventHandlers[$eventType][] = $eventHandler;
+    }
+
+    /**
+     * Removes an event handler
+     *
+     * @param string                         $eventType
+     * @param \YapepBase\Event\IEventHandler $eventHandler
+     */
+    public function removeEventHandler($eventType, IEventHandler $eventHandler) {
+        if (
+            !empty($this->eventHandlers[$eventType])
+            && false !== ($key = array_search($eventHandler, $this->eventHandlers[$eventType], true))
+        ) {
+            unset($this->eventHandlers[$eventType][$key]);
+        }
+    }
+
+    /**
+     * Returns an array containing all the event handlers registered to the event type
+     *
+     * @param string $eventType
+     *
+     * @return array
+     */
+    public function getEventHandlers($eventType) {
+        return (isset($this->eventHandlers[$eventType]) ? $this->eventHandlers[$eventType] : array());
+    }
+
+    /**
+     * Raises an event
+     *
+     * @param Event $event
+     */
+    public function raise(Event $event) {
+        $type = $event->getType();
+        if (empty($this->eventHandlers[$type])) {
+            return;
+        }
+        foreach($this->eventHandlers[$type] as $handler) {
+            $handler->handleEvent($event);
+        }
+    }
+}
