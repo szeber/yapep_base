09dc40cd5e4e991c7f0a04ec7cc67b591c9a8c99
Zsolt Szeberenyi <z.szeberenyi@ixolit.com>
2011-12-15 09:18:42 +0000

    Added random class from Janoszen

---
+++ Test/Util/RandomTest.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * This file is part of YAPEPBase.
+ *
+ * @package      YapepBase
+ * @subpackage   Test\Util
+ * @author       Zsolt Szeberenyi <szeber@yapep.org>
+ * @copyright    2011 The YAPEP Project All rights reserved.
+ * @license      http://www.opensource.org/licenses/bsd-license.php BSD License
+ */
+
+namespace YapepBase\Test\Util;
+
+/**
+ * RandomTest class
+ *
+ * @package    YapepBase
+ * @subpackage Test\Util
+ * @covers \YapepBase\Util\Random
+ */
+use YapepBase\Exception\Exception;
+
+use YapepBase\Util\Random;
+
+class RandomTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests using the \mt_rand() function
+	 */
+	public function testPseudoNumberMT() {
+		if (!\function_exists('\mt_rand')) {
+			$this->skipTest('\mt_rand() is not available');
+		}
+		$number = Random::pseudoNumber(0, 1, Random::NUMBER_METHOD_MTRAND);
+		$this->assertEquals('double', \gettype($number));
+		$this->assertGreaterThanOrEqual(0, $number);
+		$this->assertLessThanOrEqual(1, $number);
+	}
+
+	/**
+	 * Tests using the \rand() function
+	 */
+	public function testPseudoNumber() {
+		$number = Random::pseudoNumber(0, 1, Random::NUMBER_METHOD_RAND);
+		$this->assertEquals('double', \gettype($number));
+		$this->assertGreaterThanOrEqual(0, $number);
+		$this->assertLessThanOrEqual(1, $number);
+	}
+
+	/**
+	 * Tests the method check
+	 */
+	public function testPseudoNumberMethodChecks() {
+	    try {
+	        Random::pseudoNumber(0, 1, 'nonexistent');
+            $this->fail('The method check fails');
+	    } catch (Exception $exception) {}
+	}
+
+	/**
+	 * Tests using the \openssl_random_pseudo_bytes() function
+	 */
+	public function testPseudoStringOpenSSL() {
+		if (!\function_exists('\openssl_random_pseudo_bytes')) {
+			$this->skipTest('\openssl_random_pseudo_bytes() is not available');
+		}
+		$string = Random::pseudoString(61, Random::STRING_METHOD_OPENSSL);
+		$this->assertEquals(61, \strlen($string));
+	}
+
+	/**
+	 * Tests using the \uniqid() function
+	 */
+	public function testPseudoStringUniqid() {
+		$string = Random::pseudoString(61, Random::STRING_METHOD_UNIQID);
+		$this->assertEquals(61, \strlen($string));
+	}
+
+	/**
+	 * Tests using the \md5(time()) functions
+	 */
+	public function testPseudoStringTime() {
+		$string = Random::pseudoString(61, Random::STRING_METHOD_TIME);
+		$this->assertEquals(61, \strlen($string));
+	}
+
+	/**
+	 * Tests the method check
+	 */
+	public function testPseudoStringMethodChecks() {
+	    try {
+	        Random::pseudoString(61, 'nonexistent');
+            $this->fail('The method check fails');
+	    } catch (Exception $exception) {}
+	}
+
+}
---
+++ Util/Random.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * This file is part of YAPEPBase.
+ *
+ * @package      YapepBase
+ * @subpackage   Util
+ * @author       Janos Pasztor <janos@janoszen.hu>
+ * @copyright    2011 The YAPEP Project All rights reserved.
+ * @license      http://www.opensource.org/licenses/bsd-license.php BSD License
+ */
+
+namespace YapepBase\Util;
+use YapepBase\Exception\Exception;
+
+/**
+ * Random class
+ *
+ * @package    YapepBase
+ * @subpackage Util
+ */
+class Random {
+
+    const NUMBER_METHOD_AUTO = 'auto';
+
+    const NUMBER_METHOD_RAND = 'rand';
+
+    const NUMBER_METHOD_MTRAND = 'mtrand';
+
+    const STRING_METHOD_AUTO = 'auto';
+
+    const STRING_METHOD_TIME = 'time';
+
+    const STRING_METHOD_UNIQID = 'uniqid';
+
+    const STRING_METHOD_OPENSSL = 'openssl';
+
+    /**
+     * Generate a pseudo-random number between $min and $max. If possible, this function will use mt_rand() for
+     * random number generation. Otherwise it will fall back to rand(), which uses the libc random number generator.
+     *
+     * @param  double $min
+     * @param  double $max
+     * @param  int    $method   Random generator method to use. If not available, fallback to auto.
+     *
+     * @return double
+     */
+    static function pseudoNumber($min = 0, $max = 1, $method = self::NUMBER_METHOD_AUTO) {
+        switch ($method) {
+            case self::NUMBER_METHOD_AUTO :
+            case self::NUMBER_METHOD_MTRAND :
+                if (\function_exists('\mt_rand')) {
+                    $value =\mt_rand();
+                    $maxvalue =\mt_getrandmax();
+                    break;
+                }
+
+            case self::NUMBER_METHOD_RAND :
+                $value =\rand();
+                $maxvalue =\getrandmax();
+                break;
+
+            default :
+                throw new Exception('Invalid method: ' . $method);
+                break;
+        }
+        return ((float)$min + $value * (((float)$max - (float)$min) / $maxvalue));
+    }
+
+    /**
+     * Generate a pseudo-random string in the given length. This string is NOT suitable for cryptographic purposes!
+     *
+     * @param  integer $length
+     * @param  integer $method   The method to use for random string generation. Falls back to default if the
+     *                           method is not available.
+     * @return string
+     *
+     * @throws \YapepBase\Exception   If the method is invalid
+     */
+    static function pseudoString($length = 23, $method = self::STRING_METHOD_AUTO) {
+        $string = '';
+        switch ($method) {
+            case self::STRING_METHOD_AUTO :
+            case self::STRING_METHOD_OPENSSL :
+                if (\function_exists('\openssl_random_pseudo_bytes')) {
+                    $string =\openssl_random_pseudo_bytes($length, $strong);
+                    break;
+                }
+
+            case self::STRING_METHOD_UNIQID :
+                while (\strlen($string) < $length) {
+                    $string .=\uniqid('', true);
+                }
+                $string =\substr($string, 0, $length);
+                break;
+
+            case self::STRING_METHOD_TIME :
+                while (\strlen($string) < $length) {
+                    $string .=\md5(\time());
+                }
+                $string =\substr($string, 0, $length);
+                break;
+
+            default :
+                throw new Exception('Invalid method: ' . $method);
+                break;
+        }
+        return $string;
+    }
+}
