fac1e964daefdf1fed49f2f04ae9ee5bb474aaa2
Janos Pasztor <j.pasztor@ixolit.com>
2012-01-30 12:29:59 +0100

    Imported exceptions from janoszen's ACR

---
+++ Exception/ValueException.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of YAPEPBase. It was merged from janoszen's Alternate-Class-Repository project.
+ *
+ * @package      YapepBase
+ * @subpackage   Exception
+ * @author       Janos Pasztor <net@janoszen.hu>
+ * @copyright    2011 The YAPEP Project All rights reserved.
+ * @license      http://www.opensource.org/licenses/bsd-license.php BSD License
+ */
+
+namespace YapepBase\Exception;
+
+/**
+ * This Exception states, that an invalid value was provided.
+ */
+class ValueException extends Exception {
+
+    /**
+     *
+     * @param mixed $object the object which does not match the required type
+     * @param string $required the type required
+     */
+    function __construct($value, $required = "") {
+        $message = 'Invalid value: ' . $value;
+        if ($required) {
+            $message .= ' expected ' . $required;
+        }
+        parent::__construct($message);
+    }
+
+}
---
+++ Test/Exception/IndexOutOfBoundsExceptionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PHP\Lang;
+
+use \YapepBase\Exception\IndexOutOfBoundsException;
+
+/**
+ *  This class tests the functionality of \YapepBase\Exception\IndexOutOfBoundsException
+ */
+class IndexOutOfBoundsExceptionTest extends \PHPUnit_Framework_TestCase {
+	/**
+	 * Tests throwing the exception with an offset.
+	 */
+	public function testThrowWithOffset() {
+		try {
+			$offset = 12;
+			throw new \YapepBase\Exception\IndexOutOfBoundsException($offset);
+			$this->fail('Exception not thrown!');
+		} catch (\YapepBase\Exception\IndexOutOfBoundsException $e) {
+			$this->assertNotEquals(false, \strpos($e->getMessage(), (string)$offset),
+					'Offset ' . $offset . ' is not in message! Message was: ' . $e->getMessage());
+		}
+	}
+
+	/**
+	 * Tests throwing the exception without an offset
+	 */
+	public function testThrowWithoutOffset() {
+		try {
+			throw new \YapepBase\Exception\IndexOutOfBoundsException();
+			$this->fail('Exception not thrown!');
+		} catch (\YapepBase\Exception\IndexOutOfBoundsException $e) {
+			$this->assertNotEquals('', $e->getMessage(), 'Message is empty!');
+		}
+	}
+}
