060d575fb6691fdeeda7f7aa730da72e0b7d8b57
Fodor Laszlo <l.fodor@ixolit.com>
2012-06-11 11:18:46 +0000

    BatchScript rewritten, it contains switch handling now

--- src/YapepBase/Batch/BatchScript.php
+++ src/YapepBase/Batch/BatchScript.php
@@ -4,16 +4,19 @@
  *
  * @package      YapepBase
  * @subpackage   Batch
- * @author       Zsolt Szeberenyi <szeber@yapep.org>
  * @copyright    2011 The YAPEP Project All rights reserved.
  * @license      http://www.opensource.org/licenses/bsd-license.php BSD License
  */
 
+// Set the ticks, so we can handle signals
+declare(ticks = 1);
+
 namespace YapepBase\Batch;
+
 use YapepBase\Application;
+use YapepBase\DependencyInjection\SystemContainer;
 use YapepBase\Event\Event;
 use YapepBase\Mime\MimeType;
-use YapepBase\DependencyInjection\SystemContainer;
 use YapepBase\View\ViewDo;
 
 /**
@@ -26,6 +29,9 @@ use YapepBase\View\ViewDo;
  */
 abstract class BatchScript {
 
+	/** Help usage. */
+	const HELP_USAGE = 'help';
+
 	/**
 	 * Stores the content type used by the script for output.
 	 *
@@ -34,25 +40,100 @@ abstract class BatchScript {
 	protected $contentType = MimeType::PLAINTEXT;
 
 	/**
-	 * Helper method to execute the script
+	 * The CliUserInterfaceHelper instance.
+	 *
+	 * @var CliUserInterfaceHelper
+	 */
+	protected $cliHelper;
+
+	/**
+	 * The index for the help usage.
+	 *
+	 * @var int
+	 */
+	protected $usageIndexes = array();
+
+	/**
+	 * The currently used usage.
+	 *
+	 * @var string
+	 */
+	protected $currentUsage;
+
+	/**
+	 * Handle signals in the signal handler.
+	 *
+	 * @var bool
+	 */
+	protected $handleSignals = false;
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		$this->cliHelper = new CliUserInterfaceHelper($this->getScriptDescription());
+		$this->setSignalHandler();
+
+		if (function_exists('pcntl_signal')) {
+			pcntl_signal(SIGTERM, array(&$this, 'handleSignal'), true);
+			pcntl_signal(SIGHUP, array(&$this, 'handleSignal'), true);
+			pcntl_signal(SIGINT, array(&$this, 'handleSignal'), true);
+		}
+	}
+
+	/**
+	 * Executes the script.
 	 *
 	 * @return void
 	 */
-	public static function run() {
+	abstract protected function execute();
+
+	/**
+	 * This function is called, if the process receives an interrupt, term signal, etc. It can be used to clean up
+	 * stuff. Note, that this function is not guaranteed to run or it may run after execution.
+	 *
+	 * @return void
+	 */
+	abstract protected function abort();
+
+	/**
+	 * Returns the script's decription.
+	 *
+	 * This method should return a the description for the script. It will be used as the script description in the
+	 * help.
+	 *
+	 * @return string
+	 */
+	abstract protected function getScriptDescription();
+
+	/**
+	 * Called before the run process.
+	 *
+	 * Please be cautious! In case of overriding, don't forget to call this method first.
+	 *
+	 * @return void
+	 */
+	protected function runBefore() {
 		$application = Application::getInstance();
 		$eventHandlerRegistry = $application->getDiContainer()->getEventHandlerRegistry();
-		try {
-			$container = $application->getDiContainer();
-			$container[SystemContainer::KEY_VIEW_DO] = $container->share(function($container) {
-				return new ViewDo(MimeType::PLAINTEXT);
-			});
+		$container = $application->getDiContainer();
+		$container[SystemContainer::KEY_VIEW_DO] = $container->share(function($container) {
+			return new ViewDo(MimeType::PLAINTEXT);
+		});
 
-			$eventHandlerRegistry->raise(new Event(Event::TYPE_APPSTART));
-			$instance = new static();
-			$instance->runScript();
-		} catch (\Exception $exception) {
-			$application->getDiContainer()->getErrorHandlerRegistry()->handleException($exception);
-		}
+		$eventHandlerRegistry->raise(new Event(Event::TYPE_APPSTART));
+	}
+
+	/**
+	 * Called after the run process.
+	 *
+	 * Please be cautious! In case of overriding, don't forget to call this method in the end.
+	 *
+	 * @return void
+	 */
+	protected function runAfter() {
+		$application = Application::getInstance();
+		$eventHandlerRegistry = $application->getDiContainer()->getEventHandlerRegistry();
 		$eventHandlerRegistry->raise(new Event(Event::TYPE_APPFINISH));
 	}
 
@@ -60,9 +141,106 @@ abstract class BatchScript {
 	 * Starts script execution.
 	 *
 	 * @return void
+	 *
+	 * @throws \Exception   On errors
 	 */
-	protected function runScript() {
-		$this->execute();
+	public function run() {
+		$this->runBefore();
+
+		$this->prepareSwitches();
+		try {
+			$this->parseSwitches($this->cliHelper->getParsedArgs());
+		}
+		catch (\Exception $exception) {
+			$this->cliHelper->setErrorMessage($exception->getMessage());
+
+			echo $this->cliHelper->getUsageOutput(false);
+			// Re-throw the exception
+			throw $exception;
+		}
+
+		try {
+			if ($this->currentUsage == self::HELP_USAGE) {
+				echo $this->cliHelper->getUsageOutput(true);
+			}
+			else {
+				$this->execute();
+			}
+		}
+		catch (\Exception $exception) {
+			$this->removeSignalHandler();
+			Application::getInstance()->getDiContainer()->getErrorHandlerRegistry()->handleException($exception);
+		}
+		$this->removeSignalHandler();
+
+		$this->runAfter();
+	}
+
+	/**
+	 * Sets the switches used by the script
+	 *
+	 * @return void
+	 */
+	protected function prepareSwitches() {
+		$this->usageIndexes[self::HELP_USAGE] = $this->cliHelper->addUsage('Help');
+
+		$this->cliHelper->addSwitch('e', null, 'Name of the execution environment', null, false, 'environment', false);
+
+		$this->cliHelper->addSwitch(null, 'help', 'Shows the help', $this->usageIndexes[self::HELP_USAGE]);
+	}
+
+	/**
+	 * Parses the switches used by the script.
+	 *
+	 * @param array $switches   The parsed switches.
+	 *
+	 * @return bool   Returns TRUE, if the validation of the provided switches was successful.
+	 *
+	 * @throws \YapepBase\Exception\ParameterException   If there are errors regarding the PID file
+	 */
+	protected function parseSwitches(array $switches) {
+		if (isset($switches['help'])) {
+			$this->currentUsage = self::HELP_USAGE;
+		}
+	}
+
+	/**
+	 * Sets the signal handlers
+	 *
+	 * @return void
+	 */
+	protected function setSignalHandler() {
+		$this->handleSignals = true;
+	}
+
+	/**
+	 * Removes the signal handlers
+	 *
+	 * @return void
+	 */
+	protected function removeSignalHandler() {
+		$this->handleSignals = false;
+	}
+
+	/**
+	 * Signal handler
+	 *
+	 * @param int $signo   The signal number.
+	 *
+	 * @return void
+	 *
+	 * @codeCoverageIgnore
+	 */
+	final public function handleSignal($signo) {
+		if ($this->handleSignals) {
+			switch ($signo) {
+				case SIGTERM:
+				case SIGHUP:
+				case SIGINT:
+					$this->abort();
+					break;
+			}
+		}
 	}
 
 	/**
@@ -79,10 +257,5 @@ abstract class BatchScript {
 		Application::getInstance()->getDiContainer()->getViewDo()->set($nameOrData, $value);
 	}
 
-	/**
-	 * Executes the script.
-	 *
-	 * @return void
-	 */
-	abstract protected function execute();
+
 }
--- test/YapepBase/Mock/Batch/BatchScriptMock.php
+++ test/YapepBase/Mock/Batch/BatchScriptMock.php
@@ -25,14 +25,14 @@ class BatchScriptMock extends BatchScript {
 	 *
 	 * @var mixed
 	 */
-	static public $closureData;
+	public $closureData;
 
 	/**
 	 * The closure that's called from the execute method.
 	 *
 	 * @var \Closure
 	 */
-	static public $executeClosure;
+	public $executeClosure;
 
 	/**
 	 * Executes the script.
@@ -40,14 +40,34 @@ class BatchScriptMock extends BatchScript {
 	 * @return void
 	 */
 	protected function execute() {
-		static::$closureData = null;
-		$closure = static::$executeClosure;
-		$closure($this);
-		static::$executeClosure = null;
+		$closure = $this->executeClosure;
+		$closure();
 	}
 
 	public function setToView($nameOrData, $value = null) {
 		parent::setToView($nameOrData, $value);
+	}
+
+	/**
+	 * Returns the script's decription.
+	 *
+	 * This method should return a the description for the script. It will be used as the script description in the
+	 * help.
+	 *
+	 * @return string
+	 */
+	protected function getScriptDescription() {
+		return 'Mock script';
+	}
+
+	/**
+	 * This function is called, if the process receives an interrupt, term signal, etc. It can be used to clean up
+	 * stuff. Note, that this function is not guaranteed to run or it may run after execution.
+	 *
+	 * @return void
+	 */
+	protected function abort() {
+
 	}
 
 }
