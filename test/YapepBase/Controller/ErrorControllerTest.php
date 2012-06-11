e4a359916dc20acdd3fc9160c8113622deb9c2d9
Zsolt Szeberenyi <szeber@yapep.org>
2012-06-11 14:54:00 +0200

    Improved error controller code coverage

--- test/YapepBase/Controller/ErrorControllerTest.php
+++ test/YapepBase/Controller/ErrorControllerTest.php
@@ -10,17 +10,18 @@
 
 namespace YapepBase\Controller;
 
-use YapepBase\Controller\DefaultErrorController;
 use YapepBase\Request\HttpRequest;
 use YapepBase\Response\HttpResponse;
 use YapepBase\Mock\Response\OutputMock;
+use YapepBase\Exception\ControllerException;
+
 /**
  * Test for the default error controller
  *
  * @package    YapepBase
  * @subpackage Test\Controller
  */
-class DefaultErrorControllerTest extends \PHPUnit_Framework_TestCase {
+class ErrorControllerTest extends \PHPUnit_Framework_TestCase {
 
 	public function testErrors() {
 		$response = null;
@@ -38,19 +39,42 @@ class DefaultErrorControllerTest extends \PHPUnit_Framework_TestCase {
 
 		$this->assertContains('Internal server error', $response->getRenderedBody(), 'Invalid output from action');
 		$this->assertSame(500, $response->getStatusCode(), 'Invalid status code after action has run');
+
+		// Not existing action test
+		$controller = $this->getController($response);
+		// Missing action will trigger an error, silence it
+		@$controller->run(403);
+
+		// Missing error code action should produce a 500 error
+		$this->assertContains('Internal server error', $response->getRenderedBody(), 'Invalid output from action');
+		$this->assertSame(500, $response->getStatusCode(), 'Invalid status code after action has run');
+
+		$controller = $this->getController($response, '\YapepBase\Mock\Controller\ErrorControllerMock');
+		$controller->actionClosure = function () {
+			return array('test');
+		};
+
+		try {
+			$controller->run(500);
+			$this->fail('The run method should thow a ControllerException');
+		} catch (ControllerException $e) {
+		}
 	}
 
 	/**
 	 * Instantiates a new controller
 	 *
-	 * @param \YapepBase\Response\HttpResponse $response   The response object. (outgoing param)
+	 * @param \YapepBase\Response\HttpResponse $response              The response object. (outgoing param)
+	 * @param string                           $controllerClassName   The name of the instantiated class
 	 *
 	 * @return \YapepBase\Controller\DefaultErrorController
 	 */
-	public function getController(&$response = null) {
+	public function getController(
+		&$response = null, $controllerClassName = '\YapepBase\Controller\DefaultErrorController'
+	) {
 		$request    = new HttpRequest(array(), array(), array(), array('REQUEST_URI' => '/'), array(), array(), false);
 		$output     = new OutputMock();
 		$response   = new HttpResponse($output);
-		return new DefaultErrorController($request, $response);
+		return new $controllerClassName($request, $response);
 	}
 }
