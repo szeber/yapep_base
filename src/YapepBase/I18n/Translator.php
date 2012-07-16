3fb1cee18d3a17f4d657ba8bfcd9756b547e836d
Laszlo Fodor <fodor.laszlo@gmail.com>
2012-07-16 17:40:15 +0200

    errorModeForMissinDictionary implemented in the Translator for developing purposes

--- src/YapepBase/I18n/Translator.php
+++ src/YapepBase/I18n/Translator.php
@@ -54,6 +54,13 @@ use YapepBase\Exception\I18n\TranslationNotFoundException;
  *         For all modes except for the exception mode, the original string will be returned with the parameters
  *         replaced in the source string.
  *     </li>
+ *     <li>
+ *         <b>errorModeForMissingDictionary</b>: Sets how to treat the error when the dictionary file is missing.
+ *         Must be set to one of the ERROR_MODE_* constants. Defaults to exception {@uses self::ERROR_MODE_EXCEPTION}
+ *         The effect is the same as at the errorMode param.
+ *         This option should be only changed at the beginning of the development,
+ *         as a missing dictionary should be treated very seriously.
+ *     </li>
  * </ul>
  * Either the paramPrefix or paramSuffix is required to be non-empty (the defaults suffice), but it's recommended to
  * set both. If both paramPrefix and paramSuffix are set to empty string, or an invalid error mode is provided a
@@ -123,6 +130,13 @@ class Translator implements ITranslator {
 	protected $errorMode;
 
 	/**
+	 * The error mode to use when a dictionary is missing.
+	 *
+	 * @var string
+	 */
+	protected $errorModeForMissingDictionary;
+
+	/**
 	 * Constructor.
 	 *
 	 * @param \YapepBase\Storage\IStorage $storage           The storage instance used to retrieve the dictionaries.
@@ -160,6 +174,8 @@ class Translator implements ITranslator {
 		$this->paramSuffix         = (string)$config->get($configBase . '.paramSuffix', '%');
 		$this->strictParamChecking = (bool)$config->get($configBase . '.strictParamChecking', false);
 		$this->errorMode           = (string)$config->get($configBase . '.errorMode', self::ERROR_MODE_EXCEPTION);
+		$this->errorModeForMissingDictionary
+			= (string)$config->get($configBase . '.errorModeForMissingDictionary', self::ERROR_MODE_EXCEPTION);
 
 		if (
 			!in_array($this->errorMode,
@@ -195,32 +211,47 @@ class Translator implements ITranslator {
 		if (!isset($this->dictionaryCache[$sourceKey])) {
 			$data = $this->storage->get($sourceKey);
 			if (empty($data)) {
-				throw new DictionaryNotFoundException('Dictionary not found for source class "' . $sourceClass
-					. '" and language "' . $language . '"');
+				$this->handleError($this->errorModeForMissingDictionary,
+					new DictionaryNotFoundException('Dictionary not found for source class "' . $sourceClass
+					. '" and language "' . $language . '"'));
 			}
 			$this->dictionaryCache[$sourceKey] = $data;
 		}
 		if (isset($this->dictionaryCache[$sourceKey][$string])) {
 			$string = $this->dictionaryCache[$sourceKey][$string];
 		} else {
-			switch ($this->errorMode) {
-				case self::ERROR_MODE_NONE:
-					// Do nothing
-					break;
-
-				case self::ERROR_MODE_ERROR:
-					trigger_error('Translation not found for source class "' . $sourceClass . '", language "'
-						. $language . '" and string: "' . $string . '"', E_USER_WARNING);
-					break;
-
-				case self::ERROR_MODE_EXCEPTION:
-				default:
-					throw new TranslationNotFoundException('Translation not found for source class "' . $sourceClass
-					. '", language "' . $language . '" and string: "' . $string . '"');
-					break;
-			}
+			$this->handleError($this->errorMode,
+				new TranslationNotFoundException('Translation not found for source class "' . $sourceClass
+					. '", language "' . $language . '" and string: "' . $string . '"'));
 		}
 		return $this->substituteParameters($string, $params);
+	}
+
+	/**
+	 * Handles the given error according to the given mode.
+	 *
+	 * @param string                         $errorMode   The error mode. {@uses self::ERROR_MODE_*}
+	 * @param \YapepBase\Exception\Exception $exception   The exception what should be used.
+	 *
+	 * @throws Exception if the given mode is for exception.
+	 *
+	 * @return void
+	 */
+	protected function handleError($errorMode, Exception $exception) {
+		switch ($errorMode) {
+			case self::ERROR_MODE_NONE:
+				// Do nothing
+				break;
+
+			case self::ERROR_MODE_ERROR:
+				trigger_error($exception->getMessage(), E_USER_WARNING);
+				break;
+
+			case self::ERROR_MODE_EXCEPTION:
+			default:
+				throw $exception;
+				break;
+		}
 	}
 
 	/**
