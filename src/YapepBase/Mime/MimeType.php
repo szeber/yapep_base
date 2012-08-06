048ef4647e3208486a9674e307d6d3ff40e11eb4
emul <laszlo@redact-media.com>
2012-08-06 09:27:33 +0200

    Image types added to the  MimeType class

--- src/YapepBase/Mime/MimeType.php
+++ src/YapepBase/Mime/MimeType.php
@@ -2,11 +2,10 @@
 /**
  * This file is part of YAPEPBase.
  *
- * @package      YapepBase
- * @subpackage   Mime
- * @author       Janos Pasztor <j.pasztor@ixolit.com>
- * @copyright    2011 The YAPEP Project All rights reserved.
- * @license      http://www.opensource.org/licenses/bsd-license.php BSD License
+ * @package    YapepBase
+ * @subpackage Mime
+ * @copyright  2011 The YAPEP Project All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php BSD License
  */
 
 
@@ -15,24 +14,41 @@ namespace YapepBase\Mime;
 /**
  * This class contains the most commonly used MIME types as constants
  *
- * @package      YapepBase
- * @subpackage   Mime
+ * @package    YapepBase
+ * @subpackage Mime
  */
 class MimeType {
-	/** HTML content type */
+
+	/** HTML content type. */
 	const HTML = 'text/html';
-	/** XHTML content type */
+
+	/** XHTML content type. */
 	const XHTML = 'application/xhtml+xml';
-	/** CSS content type */
+
+	/** CSS content type. */
 	const CSS = 'text/css';
-	/** Javascript content type */
+
+	/** Javascript content type. */
 	const JAVASCRIPT = 'appliation/javascript';
-	/** JSON content type */
+
+	/** JSON content type. */
 	const JSON = 'application/json';
-	/** XML content type */
+
+	/** XML content type. */
 	const XML = 'application/xml';
-	/** Plain text content type */
+
+	/** Plain text content type. */
 	const PLAINTEXT = 'text/plain';
-	/** Octet stream content type */
+
+	/** Octet stream content type. */
 	const OCTET_STREAM = 'application/octet-stream';
+
+	/** Image type: PNG. */
+	const IMAGE_PNG = 'image/png';
+
+	/** Image type: JPEG. */
+	const IMAGE_JPEG = 'image/jpeg';
+
+	/** Image type: GIF. */
+	const IMAGE_GIF = 'image/gif';
 }
