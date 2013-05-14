dc4ce06e1fdd6115508d73b987faa9eda9edbf90
szeber <zsolt@redact-media.com>
2013-05-14 13:28:14 +0200

    Fixed tests
    --HG--
    branch : v0.3.x

--- test/YapepBase/Mock/Debugger/DebuggerMock.php
+++ test/YapepBase/Mock/Debugger/DebuggerMock.php
@@ -11,6 +11,7 @@
 namespace YapepBase\Mock\Debugger;
 
 use YapepBase\Debugger\IDebugger;
+use YapepBase\Debugger\Item\IDebugItem;
 
 /**
  * Mock object for debugger testing
@@ -21,92 +22,23 @@ use YapepBase\Debugger\IDebugger;
 class DebuggerMock implements IDebugger {
 
 	/**
-	 * Stores a timing with the given name.
+	 * Returns the time when the request was started as a float timestamp (microtime).
 	 *
-	 * @param string $name   The name of the timing.
-	 *
-	 * @return void
+	 * @return float
 	 */
-	public function addClockMilestone($name) {
-		// TODO: Implement addClockMilestone() method.
+	public function getStartTime() {
+		// TODO: Implement getStartTime() method.
 	}
 
 	/**
-	 * Stores a memory usage with the given name.
+	 * Adds a new debug item to the debugger.
 	 *
-	 * @param string $name   The name of the measure.
-	 *
-	 * @return void
-	 */
-	public function addMemoryUsageMilestone($name) {
-		// TODO: Implement addMemoryUsageMilestone() method.
-	}
-
-	/**
-	 * Stores a message.
-	 *
-	 * @param mixed $message   The message that should be stored.
+	 * @param \YapepBase\Debugger\Item\IDebugItem $item   The debug item.
 	 *
 	 * @return void
 	 */
-	public function logMessage($message) {
-		// TODO: Implement logMessage() method.
-	}
-
-	/**
-	 * Logs and error.
-	 *
-	 * @param int    $code      The code of the error.
-	 * @param string $message   Error message.
-	 * @param string $file      The name of the file, where the error occured.
-	 * @param int    $line      The number of the line where the error occured.
-	 * @param array  $context   The variables from the actual context.
-	 * @param array  $trace     The backtrace for the error.
-	 * @param string $id        The id of the error.
-	 *
-	 * @return void
-	 */
-	public function logError(
-		$code, $message, $file = '', $line = 0, array $context = array(), array $trace = array(), $id = ''
-	) {
-		// TODO: Implement logError() method.
-	}
-
-	/**
-	 * Logs the given query.
-	 *
-	 * @param string $type             The type of the query {@uses IDebugger::QUERY_TYPE_*}.
-	 * @param string $connectionName   Connection identification string with backend type.
-	 * @param string $query            The query string.
-	 * @param mixed  $params           The params used by the query.
-	 *
-	 * @return int   The id of the query, which can be used to measure the execution time of it.
-	 */
-	public function logQuery($type, $connectionName, $query, $params = null) {
-	// TODO: Implement logQuery() method.
-	}
-
-	/**
-	 * Logs the timing of the given query.
-	 *
-	 * @param string $type            The type of the query {@uses IDebugger::QUERY_TYPE_*}.
-	 * @param int    $queryId         The id of the query.
-	 * @param float  $executionTime   The execution time of the query.
-	 * @param mixed  $params          The params what used by the query.
-	 *
-	 * @return void
-	 */
-	public function logQueryExecutionTime($type, $queryId, $executionTime, $params = null) {
-		// TODO: Implement logQueryExecutionTime() method.
-	}
-
-	/**
-	 * Displays the interface of the Debugger (if it has one).
-	 *
-	 * @return void
-	 */
-	public function display() {
-		// TODO: Implement display() method.
+	public function addItem(IDebugItem $item) {
+		// TODO: Implement addItem() method.
 	}
 
 	/**
