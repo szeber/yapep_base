92826ddf9896e3789d1852ce7fd02cd924ebd521
szeber <zsolt@redact-media.com>
2013-05-21 14:22:30 +0200

    Added the option of disabling the OB in the response
    --HG--
    branch : v0.3.x

--- test/YapepBase/Mock/Response/ResponseMock.php
+++ test/YapepBase/Mock/Response/ResponseMock.php
@@ -32,6 +32,34 @@ class ResponseMock implements \YapepBase\Response\IResponse {
 	}
 
 	/**
+	 * Sets whether output buffering should be enabled or not.
+	 *
+	 * By default output buffering is enabled. If disabling this, the output will be echoed instead of using the
+	 * output object to send it, so the response object has no control over it. This may cause problems with the
+	 * sending of headers for example.
+	 * If disabling the buffering it will flush and disable all output buffers that were created after
+	 * the initialization of the response object. It will also send all headers that were added.
+	 *
+	 * @param bool $isEnabled   If TRUE, enables, if FALSE disables the output buffering.
+	 *
+	 * @return mixed
+	 *
+	 * @throws \YapepBase\Exception\ParameterException   If the output buffering is already in the specified status.
+	 */
+	public function setOutputBufferingStatus($isEnabled) {
+
+	}
+
+	/**
+	 * Returns TRUE if the output buffering is enabled, FALSE if it's not.
+	 *
+	 * @return bool
+	 */
+	public function getOutputBufferingStatus() {
+
+	}
+
+	/**
 	 * Clears all previous, not sent output in the buffer.
 	 *
 	 * @return void
