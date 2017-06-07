0df4db72cf471d75f64f7aeb4d946b12f424c8d6
emul <fodor.laszlo@gmail.com>
2017-06-07 14:13:25 +0200

    Added helping methods to DaoAbstract

--- src/YapepBase/Dao/DaoAbstract.php
+++ src/YapepBase/Dao/DaoAbstract.php
@@ -10,6 +10,9 @@
 
 namespace YapepBase\Dao;
 
+
+use YapepBase\Database\DbConnection;
+
 /**
  * Base abstract DAO which should be extended by every DAO class.
  *
@@ -17,10 +20,112 @@ namespace YapepBase\Dao;
  * @subpackage Dao
  */
 abstract class DaoAbstract {
+
 	/** Interval unit for day. */
 	const INTERVAL_UNIT_DAY = 'day';
 	/** Interval unit for month. */
 	const INTERVAL_UNIT_MONTH = 'month';
 	/** Interval unit for year. */
 	const INTERVAL_UNIT_YEAR = 'year';
+
+	/**
+	 * Generates a condition like [tableAlias].[fieldName] = [expectedValue]
+	 *
+	 * @param DbConnection $dbConnection              DB Connection.
+	 * @param string       $fieldName                 The field to check.
+	 * @param mixed        $expectation               What value should be the field matched against.
+	 * @param array        $conditions                Array which holds the conditions.
+	 * @param array        $queryParams               Array which holds the query params.
+	 * @param string       $tableAlias                Alias of the table which contains the given field.
+	 * @param bool         $onlyNullConsideredEmpty   If TRUE only null value will be considered empty
+	 */
+	protected function getEqualityCondition(
+		DbConnection $dbConnection, $fieldName, $expectation, array &$conditions, array &$queryParams,
+		$tableAlias = '', $onlyNullConsideredEmpty = false
+	) {
+		if (
+			(!$onlyNullConsideredEmpty && empty($expectation))
+			||
+			($onlyNullConsideredEmpty && is_null($expectation))
+		) {
+			return;
+		}
+
+		if (is_bool($expectation)) {
+			$expectation = (int)$expectation;
+		}
+
+		$paramName = $this->getParamName($fieldName, $tableAlias);
+		$fieldName = $this->getPrefixedField($tableAlias, $fieldName);
+
+		$conditions[] = $fieldName . ' = :' . $dbConnection->getParamPrefix() . $paramName;
+		$queryParams[$paramName] = $expectation;
+	}
+
+	/**
+	 * Generates a condition like [tableAlias].[fieldName] IN ([list])
+	 *
+	 * @param DbConnection $dbConnection   DB Connection.
+	 * @param string       $fieldName      The field to check.
+	 * @param array        $list           List of the possible values
+	 * @param array        $conditions     Array which holds the conditions.
+	 * @param array        $queryParams    Array which holds the query params.
+	 * @param string       $tableAlias     Alias of the table which contains the given field.
+	 */
+	protected function getInListCondition(
+		DbConnection $dbConnection, $fieldName, array $list, array &$conditions, array &$queryParams, $tableAlias = ''
+	) {
+		if (empty($list)) {
+			return;
+		}
+
+		$paramPrefix = $dbConnection->getParamPrefix();
+		$paramNames = [];
+		foreach ($list as $index => $item) {
+			$paramName = $this->getParamName($fieldName, $tableAlias, $index);
+
+			$paramNames[] = ':' . $paramPrefix . $paramName;
+			$queryParams[$paramName] = $item;
+		}
+
+		$conditions[] = $this->getPrefixedField($tableAlias, $fieldName) . ' IN (' . implode(', ', $paramNames) . ')';
+	}
+
+	/**
+	 * Returns the name of the given field with the proper prefix.
+	 *
+	 * @param string $tableAlias   Alias of the table.
+	 * @param string $fieldName    Name of the field.
+	 *
+	 * @return string
+	 */
+	protected function getPrefixedField($tableAlias, $fieldName) {
+		$prefix = empty($tableAlias) ? '' : $tableAlias . '.';
+
+		return $prefix . $fieldName;
+	}
+
+	/**
+	 * Creates a parameter name based on the given field and table.
+	 *
+	 * @param string   $fieldName    Name of the field.
+	 * @param string   $tableAlias   Alias of the table
+	 * @param int|null $index        An index number if the same field and table is used more than once.
+	 *
+	 * @return string
+	 */
+	protected function getParamName($fieldName, $tableAlias = '', $index = null) {
+		$paramNameParts = [];
+
+		if (!empty($tableAlias)) {
+			$paramNameParts[] = $tableAlias;
+		}
+		$paramNameParts[] = $fieldName;
+
+		if (!is_null($index)) {
+			$paramNameParts[] = $index;
+		}
+
+		return implode('_', $paramNameParts);
+	}
 }
